ef932eab02601db553b2e764cd0c8a3ae0d9f29f
Abdullah Khasawneh <abdullahkhasawneh@hotmail.com>
2022-08-31 07:56:24 +0900

    changed  to * inside each per_sample_outs folder

--- imm.sh
+++ imm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #This is a script to run the first couple of steps of Change-O in a folder containing Cell Ranger results (in separate folders). Should be generalizable for now.
-
-for d in */; do
-	cd d/outs/per_sample_outs/d/vdj_b;
+root=$(pwd)
+for d in *; do
+	cd $root/$d/outs/per_sample_outs/*/vdj_b;
 	AssignGenes.py igblast -s filtered_contig.fasta -b ~/yard/share/igblast/ --organism human --loci ig --format  blast;
-	MakeDB.py igblast -i filtered_contig_igblast.fmt7 -s filtered_contig.fasta -r ~/yard/share/germlines/imgt/human/vdj/imgt_human_*fasta --10x filtered_contig_annotations.csv --extended;
+	MakeDb.py igblast -i filtered_contig_igblast.fmt7 -s filtered_contig.fasta -r ~/yard/share/germlines/imgt/human/vdj/imgt_human_*fasta --10x filtered_contig_annotations.csv --extended;
 done
 
 exit 0;
