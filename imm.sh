28f3ed43cefaa09800ed700af930d175fe01df2e
Abdullah Khasawneh <abdullahkhasawneh@hotmail.com>
2022-09-01 09:28:55 +0900

    modified imm.sh so it works from this repository's directory

--- imm.sh
+++ imm.sh
@@ -2,9 +2,10 @@
 
 #This is a script to run the first couple of steps of Change-O in a folder containing Cell Ranger results (in separate folders). Should be generalizable for now.
 root=$(pwd)
+cd from_cellranger
 for d in *; do
 	#Preparing AIRR-formatted data:
-	cd $root/$d/outs/per_sample_outs/*/vdj_b;
+	cd $d/vdj_b;
 	AssignGenes.py igblast -s filtered_contig.fasta -b ~/yard/share/igblast/ --organism human --loci ig --format  blast;
 	MakeDb.py igblast -i filtered_contig_igblast.fmt7 -s filtered_contig.fasta -r ~/yard/share/germlines/imgt/human/vdj/imgt_human_*fasta --10x filtered_contig_annotations.csv --extended;
 
@@ -13,6 +14,6 @@ for d in *; do
 	ParseDb.py select -d filtered_contig_igblast_db-pass.tsv -f locus -u "IG[LK]" --logic all --regex --outname light 
 
 done
-
+cd $root
 exit 0;
 
